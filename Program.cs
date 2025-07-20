658be0e493170f39374245807b950c2ac9fe7c0a
peterJack7 <pnj90047@gmail.com>
2025-07-20 21:09:27 +0530

    fixed passowrd hash

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using AuthService.Application.Interfaces;
+using AuthService.Infrastructure.Data;
 using AuthService.Infrastructure.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -9,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
